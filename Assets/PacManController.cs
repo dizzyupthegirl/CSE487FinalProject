867809e25dbeda645072a43e8b33dbbb9a0d54c5
dizzyupthegirl <wolfsp@miamioh.edu>
2015-04-28 16:16:22 -0400

    Working on pacman movements

---
+++ Assets/PacManController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacManController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(-Vector3.right * Time.deltaTime*3);
+
+//		if (Input.GetAxisRaw ("Horizontal") > 0) {
+//			transform.Rotate (0, 90, 0);
+//		} else {
+//		
+//			transform.Rotate (0, -90, 0);
+//		}
+	}
+}
