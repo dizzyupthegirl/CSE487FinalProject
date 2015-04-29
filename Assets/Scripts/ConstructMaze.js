9d802b7596179c95770d923dbf9fe9f1b7319ee8
db4soundman <db4soundman@gmail.com>
2015-04-29 16:41:09 -0400

    Providing Sam a maze to Pacman to run around on.

--- Assets/Scripts/ConstructMaze.js
+++ Assets/Scripts/ConstructMaze.js
@@ -5,11 +5,11 @@ var superPrefab : Transform;
 
 function Awake () {
     var map = mapAsset.text.Split ("\n"[0]);
-    var v = new Vector3 ();
+    var v = gameObject.transform.position;
     v.y = 1.0;
     var j_off = map.length / 2.0;
     for (var j = 0; j < map.length; j ++) {
-        v.z = (map.length - j - j_off - 1) * 2;
+        v.z = (j - j_off) * 2 + 1;
         var i_off = map[j].length / 2.0;
         for (var i = 0; i < map[j].length; i ++) {
             v.x = (i - i_off) * 2 + 1;
@@ -17,9 +17,11 @@ function Awake () {
                 var inst = Instantiate (blockPrefab, v, Quaternion.identity);
                 inst.transform.parent = transform;
             } else if (map[j][i] == ".") {
-                Instantiate (pelletPrefab, v, Quaternion.identity);
+                 var inst1 =Instantiate (pelletPrefab, v, Quaternion.identity);
+                 inst1.transform.parent = transform;
             } else if (map[j][i] == "O") {
-                Instantiate (superPrefab, v, Quaternion.identity);
+                 var inst2 =Instantiate (superPrefab, v, Quaternion.identity);
+                 inst2.transform.parent = transform;
             }
         }
     }
