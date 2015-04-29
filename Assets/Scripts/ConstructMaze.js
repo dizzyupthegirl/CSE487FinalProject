40f182496dab1d752b837214f4269aa08685dba1
db4soundman <db4soundman@gmail.com>
2015-04-29 17:32:16 -0400

    Maze now centers on terrain.

--- Assets/Scripts/ConstructMaze.js
+++ Assets/Scripts/ConstructMaze.js
@@ -2,26 +2,25 @@ var mapAsset : TextAsset;
 var blockPrefab : Transform;
 var pelletPrefab : Transform;
 var superPrefab : Transform;
-
+var terrain : Terrain;
+//http://www.tosos.com/PacManClone.zip
 function Awake () {
     var map = mapAsset.text.Split ("\n"[0]);
-    var v = gameObject.transform.position;
+    var v = new Vector3 ();
     v.y = 1.0;
-    var j_off = map.length / 2.0;
+    var j_off = terrain.terrainData.size.z / 2.0;
     for (var j = 0; j < map.length; j ++) {
-        v.z = (j - j_off) * 2 + 1;
-        var i_off = map[j].length / 2.0;
+        v.z = (terrain.terrainData.size.z - j - j_off - 1) * 2;
+        var i_off = terrain.terrainData.size.x / 2.0;
         for (var i = 0; i < map[j].length; i ++) {
-            v.x = (i - i_off) * 2 + 1;
+            v.x = (-1 * terrain.terrainData.size.x / 2 + i + i_off) * 2 + i_off/4;
             if (map[j][i] == "X") {
                 var inst = Instantiate (blockPrefab, v, Quaternion.identity);
                 inst.transform.parent = transform;
             } else if (map[j][i] == ".") {
-                 var inst1 =Instantiate (pelletPrefab, v, Quaternion.identity);
-                 inst1.transform.parent = transform;
+                Instantiate (pelletPrefab, v, Quaternion.identity);
             } else if (map[j][i] == "O") {
-                 var inst2 =Instantiate (superPrefab, v, Quaternion.identity);
-                 inst2.transform.parent = transform;
+                Instantiate (superPrefab, v, Quaternion.identity);
             }
         }
     }
