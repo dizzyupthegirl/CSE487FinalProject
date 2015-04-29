6580c00c16fc58d1c82e5a05c2f7dda1bb9facaf
dizzyupthegirl <wolfsp@miamioh.edu>
2015-04-29 16:17:48 -0400

    PacMan eats cookies. Nom nom nom

---
+++ Assets/CookieEater.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CookieEater : MonoBehaviour {
+	//public GUIText scoreText;
+	public int smallCookieScore=20;
+	public int superCookieScore=100;
+	int score=0;
+	// Use this for initialization
+	void Start () {
+		//scoreText.text = "Score: 0";
+	}
+
+
+
+	void OnTriggerEnter (Collider other) {
+		print ("Tag Trigger Entered: " + other.tag);
+		if (other.tag=="Cookie") {
+			score += smallCookieScore;
+		} else if (other.tag == "SuperCookie") {
+			score += superCookieScore;
+		}
+		Destroy (other.gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//scoreText.text = "Score: " + score;
+	}
+}
