ce227f8cdf40ff0e244d9fe858d657b1ac6c1efc
dizzyupthegirl <wolfsp@miamioh.edu>
2015-05-03 12:52:45 -0400

    Stopping for now
    Working on the high score stuff sort of. Stopping for now, until meet
    with Doug

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void StoreHighscore(int newHighscore)
+	{
+		int oldHighscore = PlayerPrefs.GetInt("highscore", 0);    
+		if(newHighscore > oldHighscore)
+			PlayerPrefs.SetInt("highscore", newHighscore);
+		PlayerPrefs.Save();
+	}
+
+}
