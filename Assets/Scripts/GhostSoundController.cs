3c17527c9263a49616cc68f49e7f692eca895a7f
db4soundman <db4soundman@gmail.com>
2015-05-03 16:23:08 -0400

    Ghosts make noise, monsters are prefabs now.

---
+++ Assets/Scripts/GhostSoundController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostSoundController : MonoBehaviour {
+
+	public AudioSource regularSound;
+	public AudioSource scaredSound;
+	// Use this for initialization
+	void Start () {
+		regularSound.Play ();
+		scaredSound.Stop ();
+		becomeScared ();
+	}
+
+	public void becomeScared() {
+		regularSound.Stop ();
+		scaredSound.Play ();
+	}
+
+	public void becomeNormal() {
+		scaredSound.Stop ();
+		regularSound.Play ();
+	}
+	
+
+}
