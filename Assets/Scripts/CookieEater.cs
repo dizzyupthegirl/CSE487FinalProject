1d67be7ca9a43923a7351c7897467b80a8e9cc7a
db4soundman <db4soundman@gmail.com>
2015-05-03 20:52:54 -0400

    Scaled down ghost size, removed pacman from map and let script handle him. Reupdated script code to where it was before the merge issues.

--- Assets/Scripts/CookieEater.cs
+++ Assets/Scripts/CookieEater.cs
@@ -11,6 +11,7 @@ public class CookieEater : MonoBehaviour {
 	public Text scoreText;
 	// Use this for initialization
 	void Start () {
+		scoreText = GameObject.Find ("ScoreText").GetComponentInChildren<Text> ();
 		scoreText.text = "Score: 0";
 	}
 
