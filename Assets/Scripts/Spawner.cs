1c013c4c0d846806c9f24cc63bb44d3c63989cfc
YangLi1 <liy28@muohio.edu>
2015-05-10 23:43:54 -0400

    Still couldn't find ghosts

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -42,57 +42,57 @@ public class Spawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		// Do some cookies for enemy 
-		cookie1 = Instantiate(cookie, new Vector3 (15.0f, 200.0f, 195.0f), Quaternion.identity) as GameObject;
+		cookie1 = Instantiate(cookie, new Vector3 (15.0f, 201.0f, 195.0f), Quaternion.identity) as GameObject;
 		cookie1.rigidbody.useGravity = true;
-		cookie2 = Instantiate (cookie, new Vector3 (15.0f, 200.0f, 150.0f), Quaternion.identity) as GameObject;
+		cookie2 = Instantiate (cookie, new Vector3 (15.0f, 201.0f, 150.0f), Quaternion.identity) as GameObject;
 		cookie1.rigidbody.useGravity = true;
-		cookie3 = Instantiate (cookie, new Vector3 (15.0f, 200.0f, 105.0f), Quaternion.identity) as GameObject;
+		cookie3 = Instantiate (cookie, new Vector3 (15.0f, 201.0f, 105.0f), Quaternion.identity) as GameObject;
 		cookie3.rigidbody.useGravity = true;
-		cookie4 = Instantiate (cookie, new Vector3 (15.0f, 200.0f, 60.0f), Quaternion.identity) as GameObject;
+		cookie4 = Instantiate (cookie, new Vector3 (15.0f, 201.0f, 60.0f), Quaternion.identity) as GameObject;
 		cookie4.rigidbody.useGravity = true;
-		cookie5 = Instantiate (cookie, new Vector3 (15.0f, 200.0f, 15.0f), Quaternion.identity) as GameObject;
+		cookie5 = Instantiate (cookie, new Vector3 (15.0f, 201.0f, 15.0f), Quaternion.identity) as GameObject;
 		cookie5.rigidbody.useGravity = true;
 
 		// Do some super cookies for enemy like above
-		superCookie1 = Instantiate(superCookie, new Vector3 (5.0f, 200.0f, 185.0f), Quaternion.identity) as GameObject;
+		superCookie1 = Instantiate(superCookie, new Vector3 (5.0f, 201.0f, 185.0f), Quaternion.identity) as GameObject;
 		superCookie1.rigidbody.useGravity = true;
-		superCookie2 = Instantiate(superCookie, new Vector3 (5.0f, 200.0f, 140.0f), Quaternion.identity) as GameObject;
+		superCookie2 = Instantiate(superCookie, new Vector3 (5.0f, 201.0f, 140.0f), Quaternion.identity) as GameObject;
 		superCookie2.rigidbody.useGravity = true;
-		superCookie3 = Instantiate(superCookie, new Vector3 (5.0f, 200.0f, 95.0f), Quaternion.identity) as GameObject;
+		superCookie3 = Instantiate(superCookie, new Vector3 (5.0f, 201.0f, 95.0f), Quaternion.identity) as GameObject;
 		superCookie3.rigidbody.useGravity = true;
-		superCookie4 = Instantiate(superCookie, new Vector3 (5.0f, 200.0f, 50.0f), Quaternion.identity) as GameObject;
+		superCookie4 = Instantiate(superCookie, new Vector3 (5.0f, 201.0f, 50.0f), Quaternion.identity) as GameObject;
 		superCookie4.rigidbody.useGravity = true;
-		superCookie5 = Instantiate(superCookie, new Vector3 (5.0f, 200.0f, 5.0f), Quaternion.identity) as GameObject;
+		superCookie5 = Instantiate(superCookie, new Vector3 (5.0f, 201.0f, 5.0f), Quaternion.identity) as GameObject;
 		superCookie5.rigidbody.useGravity = true;
 
 		// Do some cookies for player
-		cookie6 = Instantiate(cookie, new Vector3 (185.0f, 200.0f, 195.0f), Quaternion.identity) as GameObject;
+		cookie6 = Instantiate(cookie, new Vector3 (185.0f, 201.0f, 195.0f), Quaternion.identity) as GameObject;
 		cookie6.rigidbody.useGravity = true;
-		cookie7 = Instantiate (cookie, new Vector3 (185.0f, 200.0f, 150.0f), Quaternion.identity) as GameObject;
+		cookie7 = Instantiate (cookie, new Vector3 (185.0f, 201.0f, 150.0f), Quaternion.identity) as GameObject;
 		cookie7.rigidbody.useGravity = true;
-		cookie8 = Instantiate (cookie, new Vector3 (185.0f, 200.0f, 105.0f), Quaternion.identity) as GameObject;
+		cookie8 = Instantiate (cookie, new Vector3 (185.0f, 201.0f, 105.0f), Quaternion.identity) as GameObject;
 		cookie8.rigidbody.useGravity = true;
-		cookie9 = Instantiate (cookie, new Vector3 (185.0f, 200.0f, 60.0f), Quaternion.identity) as GameObject;
+		cookie9 = Instantiate (cookie, new Vector3 (185.0f, 201.0f, 60.0f), Quaternion.identity) as GameObject;
 		cookie9.rigidbody.useGravity = true;
-		cookie10 = Instantiate (cookie, new Vector3 (185.0f, 200.0f, 15.0f), Quaternion.identity) as GameObject;
+		cookie10 = Instantiate (cookie, new Vector3 (185.0f, 201.0f, 15.0f), Quaternion.identity) as GameObject;
 		cookie10.rigidbody.useGravity = true;
 
 		// Do some super cookies for player
-		superCookie6 = Instantiate(superCookie, new Vector3 (195.0f, 200.0f, 185.0f), Quaternion.identity) as GameObject;
+		superCookie6 = Instantiate(superCookie, new Vector3 (195.0f, 201.0f, 185.0f), Quaternion.identity) as GameObject;
 		superCookie6.rigidbody.useGravity = true;
-		superCookie7 = Instantiate(superCookie, new Vector3 (195.0f, 200.0f, 140.0f), Quaternion.identity) as GameObject;
+		superCookie7 = Instantiate(superCookie, new Vector3 (195.0f, 201.0f, 140.0f), Quaternion.identity) as GameObject;
 		superCookie7.rigidbody.useGravity = true;
-		superCookie8 = Instantiate(superCookie, new Vector3 (195.0f, 200.0f, 95.0f), Quaternion.identity) as GameObject;
+		superCookie8 = Instantiate(superCookie, new Vector3 (195.0f, 201.0f, 95.0f), Quaternion.identity) as GameObject;
 		superCookie8.rigidbody.useGravity = true;
-		superCookie9 = Instantiate(superCookie, new Vector3 (195.0f, 200.0f, 50.0f), Quaternion.identity) as GameObject;
+		superCookie9 = Instantiate(superCookie, new Vector3 (195.0f, 201.0f, 50.0f), Quaternion.identity) as GameObject;
 		superCookie9.rigidbody.useGravity = true;
-		superCookie10 = Instantiate(superCookie, new Vector3 (195.0f, 200.0f, 5.0f), Quaternion.identity) as GameObject;
+		superCookie10 = Instantiate(superCookie, new Vector3 (195.0f, 201.0f, 5.0f), Quaternion.identity) as GameObject;
 		superCookie10.rigidbody.useGravity = true;
 
 		// Put our pac man into the scene
 		firstPersonplayer = Instantiate (enemy, new Vector3 (150.0f, 200.0f, 100.0f), Quaternion.identity) as GameObject;
-		pacMan = GameObject.Find ("PacMan");
-		pacMan.rigidbody.useGravity = true;
+		firstPersonplayer.AddComponent<Rigidbody> ();
+		firstPersonplayer.rigidbody.useGravity = true;
 
 		// Put evil pac man into the scene
 		evilEnemy = Instantiate (player, new Vector3 (50.0f, 200.0f, 100.0f), Quaternion.identity) as GameObject;
